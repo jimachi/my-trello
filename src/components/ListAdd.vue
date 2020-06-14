c94a8437676c5f1f3856650a952e530c57ef8745
Yuichiro Iijima <jimaichiro@gmail.com>
2020-06-14 16:21:05 +0900

    feature: タイトルテキストにフォーカスと当てるとCSSが変更するように変更

--- src/components/ListAdd.vue
+++ src/components/ListAdd.vue
@@ -1,9 +1,11 @@
 <template>
-  <form class="addlist" @submit.prevent="addList">
+  <form :class="classList" @submit.prevent="addList">
     <input type="text"
       v-model="title"
       class="text-input"
       placeholder="Add new List"
+      @focusin="startEditing"
+      @focusout="finishEditing"
     >
     <button type="submit" class="add-button">Add</button>
   </form>
@@ -13,13 +15,27 @@
 export default {
   data () {
     return {
-      title: ''
+      title: '',
+      isEdting: false 
+    }
+  },
+  computed: {
+    classList () {
+      const classList = ['addlist']
+      if (this.isEdting) classList.push('active')
+      return classList
     }
   },
   methods: {
     addList () {
       this.$store.dispatch('addlist', { title: this.title })
       this.title = ''
+    },
+    startEditing () {
+      this.isEdting = true
+    },
+    finishEditing () {
+      this.isEdting = false
     }
   }
 }
