bf141e3a28e8b598797b480dcff14a9351e9ca54
Yuichiro Iijima <jimaichiro@gmail.com>
2020-06-14 16:45:24 +0900

    add: Listの追加

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -3,17 +3,31 @@
     <header>my trello</header>
     <main>
       <p class="info-line">All: 0 tasks</p>
-      <list-add />
+      <div class="list-index">
+        <list v-for="(item, index) in lists"
+          :key="item.id"
+          :title="item.title"
+          :listIndex="index"
+        />
+        <list-add />
+      </div>
     </main>
   </div>
 </template>
 
 <script>
+import List from './List'
 import ListAdd from './ListAdd'
+
+import { mapState } from 'vuex'
 
 export default {
   components: {
+    List,
     ListAdd
+  },
+  computed: {
+    ...mapState(['lists'])
   }
 }
 </script>
