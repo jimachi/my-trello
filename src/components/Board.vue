ba79f32ddcdb9f84ba978a2af8e8496d6255cca0
Yuichiro Iijima <jimaichiro@gmail.com>
2020-06-14 16:12:38 +0900

    add: ListAdd

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -3,12 +3,17 @@
     <header>my trello</header>
     <main>
       <p class="info-line">All: 0 tasks</p>
+      <list-add />
     </main>
   </div>
 </template>
 
 <script>
+import ListAdd from './ListAdd'
+
 export default {
-  
+  components: {
+    ListAdd
+  }
 }
 </script>
